efca64c602cf9ae0146e2cf04fef95c5d289dbb9
nafihkk <muhammadnafihkk786@gmail.com>
2024-10-25 06:07:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import LoginRegister from './Components/LoginRegister'
+const App = () => {
+  return (
+    <div>
+      <LoginRegister/>
+    </div>
+  )
+}
+
+export default App
